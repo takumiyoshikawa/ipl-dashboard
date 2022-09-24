b60ff6468c3abf0a351363914e395d207ed818e9
takumiyoshikawa <happyriver157@gmail.com>
2022-09-25 02:54:29 +0900

    feat: add matchPage

--- src/frontend/src/pages/matchPage.tsx
+++ src/frontend/src/pages/matchPage.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { match } from "./teamPage";
 
 export const MatchPage = () => {
+    const [matches, setMatches] = useState<match[]>();
+    const { teamName, year } = useParams();
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+            const response = fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data: match[] = await (await response).json();
+            setMatches(data);
+        };
+
+        fetchMatches();
+    }, []);
+
     return (
-        <h1>Match Page</h1>
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+            {teamName && matches && matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)}
+        </div>
     )
 }
